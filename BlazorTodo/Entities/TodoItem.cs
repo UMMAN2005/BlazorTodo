5400f76f95dca9860075f7a397469fc6100bb7d6
Umman Mammadov <ummanmemmedov2005@gmail.com>
2024-07-17 10:55:08 +0400

    Files added

---
+++ BlazorTodo/Entities/TodoItem.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorTodo.Entities {
+  public class TodoItem {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+  }
+}
